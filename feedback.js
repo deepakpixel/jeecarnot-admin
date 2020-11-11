7b7ef009993598eebf0d62702ec168cdba68fc73
deepakpixel <gtx.addicted@gmail.com>
2020-11-11 23:05:34 +0530

    first commit

---
+++ feedback.js
@@ -0,0 +1,351 @@
+var obj=[
+    {
+    "feedbackId": "1",
+    "name": "Mentee Name",
+    "mentorid": "121",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+    "status": "critical"
+    },
+    {"feedbackId": "2",
+    "name": "Mentee Name",
+    "mentorid": "122",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 4,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "no"
+    },
+    "status": "pending"
+    },
+    {
+        "feedbackId": "3",
+        "name": "Mentee Name 3",
+        "mentorid": "123",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+        "status": "resolved"
+    },
+    {
+        "feedbackId": "4",
+        "name": "Mentee Name",
+        "mentorid": "124",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+        "status": "critical"
+    },
+    {
+        "feedbackId": "5",
+        "name": "Mentee Name 2",
+        "mentorid": "125",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+        "status": "pending"
+    },
+    {
+        "feedbackId": "6",
+        "name": "Mentee Name",
+        "mentorid": "126",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+        "status": "critical"
+    },
+    {
+        "feedbackId": "7",
+        "name": "Mentee Name 2",
+        "mentorid": "127",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+        "status": "pending"
+    },
+    {
+        "feedbackId": "8",
+        "name": "Mentee Name 3",
+        "mentorid": "128",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+        "status": "resolved"
+    },
+    {
+        "feedbackId": "9",
+        "name": "Mentee Name",
+        "mentorid": "129",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+        "status": "critical"
+    },
+    {
+        "feedbackId": "10",
+        "name": "Mentee Name 2",
+        "mentorid": "120",
+    "feedbackDate": "3 Aug 2020",
+    "mentorName": "Mentor Name",
+    "feedback": {
+        "effort": [
+            1,
+            1
+        ],
+        "cont": [
+            1,
+            4,
+            1,
+            2,
+            3
+        ],
+        "mentor": [
+            4,
+            4,
+            4,
+            4
+        ],
+        "resp": [
+            2,
+            3,
+            4
+        ],
+        "carnot": 5,
+        "comment": "This is the answer to first question, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "recom": "yes"
+    },
+        "status": "pending"
+    }
+];
